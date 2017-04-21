680cf589d3ff7b7b35af74733438abd6a938349e
aspramsh <aspramshadyan@gmail.com>
2017-04-22 02:16:32 +0400

    Add files via upload

--- SQL StoredProc/Filter By ID.sql
+++ SQL StoredProc/Filter By ID.sql
@@ -2,9 +2,12 @@ CREATE PROCEDURE FilterByIDs
 AS
 Begin
 if exists(select * from IDs)
-Select * from FilteredByMessages where exists(select * from IDs where
-FilteredByMessages.ID = ID)
+Select * from FilteredByMessages where exists(select * from IDs where exists(select * from 
+TimeRange Where FilteredByMessages.ID = ID and FilteredByMessages.LogTime > StartTime and 
+FilteredByMessages.LogTime < EndTime))
 else
-select * from FilteredByMessages;
+select * from FilteredByMessages where exists(select * from TimeRange where 
+FilteredByMessages.LogTime > StartTime and 
+FilteredByMessages.LogTime < EndTime);
 End;
 Go
